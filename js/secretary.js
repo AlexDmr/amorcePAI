6b84e8a03c8ed181eb4901c36859fc5c8051cf0d
Célia Ek <c.ek.luv@gmail.com>
2016-03-17 14:19:22 +0100

    modif

--- js/secretary.js
+++ js/secretary.js
@@ -1,58 +1,49 @@
 require( "./secretary.css" );
-
+//prétraitement
 var utils = require("./utils.js");
-var cabinetJS = {patientsNonAffectés:[], infirmiers:{}};
+var cabinetJS = {patientNonAffectes : [], infirmiers : {} };
+utils.XHR('GET', 'data/cabinetInfirmier.xml').then(function(xhr) {console.log(xhr);
+                                                                        var parser = new DOMParser();
+                                                                        var doc = parser.parseFromString(xhr.responseText, "text/xml");
+                                                                        var infirmiersXML = doc.querySelectorAll("infirmier");
+                                                                        var infirmierXML , i , infirmier;
+                                                                        for(i=0; i<infirmiersXML.length ; i++) {  //voir avec foreach
+                                                                            infirmierXML = infirmiersXML[i];
+                                                                            var id = infirmierXML.getAttribute('id');
+                                                                            infirmier = {   id : id,
+                                                                                            name : infirmierXML.querySelector("nom").textContent,
+                                                                                            forname : infirmierXML.querySelector("prenom").textContent,
+                                                                                            photo : infirmierXML.querySelector("photo").textContent,
+                                                                                            patients : [] 
+                                                                                        };
+                                                                            cabinetJS.infirmiers[id] = infirmier;
+                                                                        }
+                                                                        //Traitement
+                                                                        var patientsXML, patientXML, patient;
+                                                                        patientsXML = doc.querySelectorAll("patient");
+                                                                        for(i=0 ; i<patientsXML.length ; i++) {
+                                                                            patientXML = patientsXML[i];
+                                                                            patient =   {   name : patientXML.querySelector("nom").textContent,
+                                                                                            forname : patientXML.querySelector("prenom").textContent,
+                                                                                            sex : patientXML.querySelector("sexe").textContent,
+                                                                                            birthday : patientXML.querySelector("naissance").textContent,
+                                                                                            numero : patientXML.querySelector("numero").textContent, 
+                                                                                            adress: {   patientfloor : patientXML.querySelector("adresse[etage]"),
+                                                                                                        numero : patientXML.querySelector("adresse[numero]"),
+                                                                                                        street : patientXML.querySelector("adresse[rue]"),
+                                                                                                        city : patientXML.querySelector("adresse[ville]"),
+                                                                                                        postalCode : patientXML.querySelector("adresse[codePostal]")
+                                                                                                        }
+                                                                                        };
+                                                                            //patient affecté?
+                                                                            var visite = patientXML.querySelector("visite[intervenant]");
+                                                                            if(visite===null){
+                                                                                cabinetJS.patientNonAffectes.push(patient);
+                                                                            } else {
+                                                                                id = visite.getAttribute("intervenant");
+                                                                                cabinetJS.infirmiers[id].patients.push(patient);
+                                                                            }
+                                                                        }
+                                                                   console.log("cabinet :",cabinetJS);
+                                                                   } );
 
-utils.XHR('GET','data/cabinetInfirmier.xml').then(function(xhr){
-	//console.log(xhr);
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(xhr.resposeText,"text/xml");
-
-    var infirmiersXML = doc.querySelectorAll("infirmier");
-    var infirmierXML;
-    var i;
-    var infirmier;
-    var id;
-    for(i=0; i<infirmiersXML.length; i++){
-    	infirmierXML=infirmiersXML[i];
-        id = infirmierXML.getAttribute('id');
-        infirmier = { name: infirmierXML.querySelector("nom").textContent,
-                      forname: infirmierXML.querySelector("prenom").textContent,
-                      photo: infirmierXML.querySelector("photo").textContent,
-                      patients:[]
-                    };
-        cabinetJS.infirmiers[id]=infirmier; //si puo mettere [id] anche se é un attributo                      
-    }
-    
-     
-    
-    //traitement
-    var patientsXML;
-    var patientXML;
-    var patient;
-    patientsXML=doc.querySelectorAll("patient");
-    for(i=0;i<patientsXML.length;i++){
-    	patientXML=patientsXML[i];
-    	patient={name: patientXML.querySelector("nom").textContent,
-    		     forname: patientXML.querySelector("prenom").textContent,
-                 sexe: patientXML.querySelector("sexe").textContent,
-                 birthdate: patientXML.querySelector("naissance").textContent,
-                 numero: patientXML.querySelector("numero").textContent,
-                 adresse: {etage:patientXML.querySelector("adresse[etage]"),
-                           numero: patientXML.querySelector("adresse[numero]"),
-                           rue: patientXML.querySelector("adresse[rue]"),
-                           ville: patientXML.querySelector("adresse[ville]"),
-                           codePostal: patientXML.querySelector("adresse[codePostale]")
-                          }
-    	        };
-    	//le patient est-il affecté?
-        var visite = patientXML.querySelector("visite[intervenant]");
-        if(visite===null){
-        	cabinetJS.patientsNonAffectés.push(patient);
-        	} else{
-        		id = visite.getAttribute("intervenant");
-        		cabinetJS.infirmier[id].patients.push(patient);
-        	}	        
-    }
-    console.log("cabinet:",cabinetJS);
-})
