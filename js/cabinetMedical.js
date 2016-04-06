7b47104ba634bd41512685c052c7cb1392a33bc9
Sylvain <cartier.sylv@gmail.com>
2016-04-06 14:26:01 +0200

    up

---
+++ js/cabinetMedical.js
@@ -0,0 +1,26 @@
+var template = require( "./cabinetMedical.html" );
+require( "./cabinetMedical.css" );
+
+module.exports = function(moduleAngular) {
+	var proxyNF = require( "./NF.js" )(moduleAngular);
+
+	var controller = function( proxyNF ) {
+        console.log( "on a une nouvelle instance de cabinetMedical" );
+        //proxyNF.getData("./data.cabinetInfirmier.xml");
+        var self = this;
+        proxyNF.getData(this.src).then( function(cabinetMedicalJS){
+            self.data = cabinetMedicalJS;
+        })
+		// Cette fonction sera appelée pour instancier un cabinet
+	}
+	controller.$inject = [ proxyNF ]; // Injection de dépendances ou services que va utiliser la fonction controller
+
+	moduleAngular.component( "cabinetMedical", {
+		template	: template,
+        controller	: controller,
+		bindings	: {
+            src     : "@",
+			titre	: "@"
+		}		
+	});
+};
